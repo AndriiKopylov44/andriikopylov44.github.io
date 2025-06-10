e4d98eb9dfede6a21fa078f5937567035c53950e
AndriiKopylov44 <jonsrwny@gmail.com>
2025-06-10 15:21:40 +0300

    modified:   index.html modified:   re8.html 	modified:   script.js 	modified:   styles.css 	modified:   tlou_1.html 	modified:   tlou_2.html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleFold() {
       const content = document.getElementById("foldableText");
-      content.style.display = content.style.display === "none" || content.style.display === "" ? "block" : "none";
+      content.style.display = content.style.display === "block" || content.style.display === "" ? "none" : "block";
     }
